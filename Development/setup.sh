243aa2d4eefbd711cf6435a106fce88feb1d1311
Tracy S. Fitch <tracy.s.fitch@gmail.com>
2020-05-31 17:28:29 -0500

    Complete creation of /etc/sudoers.d/20_wanem_user; Remove old .out and.bkp files

--- Development/setup.sh
+++ Development/setup.sh
@@ -10,7 +10,6 @@ if [[ $EUID > 0 ]]
 		exit 99 
 	else
 		echo "Initiating setup process" 
-		echo
 fi
 
 # locate function 
@@ -26,7 +25,7 @@ do_locate () {
 		echo $( eval "echo \$$__resultvar" )
 		declare -x $__resultvar
 	else
-		echo "    ! Unable to find '$1'."
+		echo "    ! Unable to find '$1'"
 		exit 99
 	fi
 }
@@ -71,6 +70,7 @@ for u in "${required_utils[@]}"
 do
 	do_locate $u
 done
+echo "  . All required command line utilities found"
 
 echo
 echo "- Learning apache server configuration"
@@ -82,21 +82,39 @@ if [ ! -z "$apache_user" ]
 then
 	echo "    = $apache_user"  
 else
-	echo "    ! Unable to identify apache user."
+	echo "    ! Unable to identify apache user"
 	exit 99
 fi
 
 # locate target directory for apache files
 echo "  - Detecting apache document root"
-target_dir=$(apachectl -S 2>&1 | sed -r -n 's/^Main\sDocumentRoot:\s\"(.*)\"/\1/p')
-if [ ! -z "$target_dir" ]
+web_dir=$(apachectl -S 2>&1 | sed -r -n 's/^Main\sDocumentRoot:\s\"(.*)\"/\1/p')
+if [ ! -z "$web_dir" ]
 then
-	echo "    = $target_dir"  
+	echo "    = $web_dir"  
 else
-	echo "    ! Unable to identify apache Main DocumentRoot."
+	echo "    ! Unable to identify apache Main DocumentRoot"
 	exit 99
 fi
 
+echo "  . Apache server detection complete"
+
+
+# checkdir function to verify directory is present and writable
+do_checkdir () {
+# parameter is directory
+	local dir=$1
+	if [ -d "$1" ] 
+	then
+		if [ ! -w "$1" ] 
+		then
+			echo "  ! $1/ not writable"
+		fi
+	else
+		echo "  ! Unable to find $1/"
+		exit 99
+	fi
+}
 
 
 # define target directory for application scripts
@@ -106,63 +124,73 @@ script_install_dir="/root"
 # write sudoers file 
 echo
 echo "- Configuring sudo"
-#php -r 'echo preg_replace_callback("/\\$([a-z0-9_]+)/i", function ($matches) { return getenv($matches[1]); }, fread(STDIN, 8192));' < ./config_templates/20_wanem_user.template > /etc/sudoers.d/20_wanem_user
-cat ./config_templates/20_wanem_user.template | envsubst > /tmp/20_wanem_user
-echo > "/tmp/20_wanem_user"
-while read line 
-do
-    line=${line/\$script_install_dir\}/$script_install_dir}
-	echo "$line" >> "/tmp/20_wanem_user"
-done < "./config_templates/20_wanem_user.template"
-cat ./config_templates/20_wanem_user.template | envsubst > /tmp/20_wanem_user
-cat <<_SUDOERS_END
+sudoers_dir="/etc/sudoers.d"
+sudoers_file="$sudoers_dir/20_wanem_user"
+do_checkdir $sudoers_dir
+if [ -f "$sudoers_file" ] && [ ! -w "$sudoers_file" ]
+then
+	echo "  ! File $sudoers_file already exists and is not writeable"
+	exit 99
+fi
+cat > "$sudoers_file" <<_SUDOERS_END
 # Give detected apache user rights for wanem required scripts and utilities
 %$apache_user         ALL=NOPASSWD:$tc_loc, $echo_loc, $mv_loc, $grep_loc, $conntrack_loc, $script_install_dir/disc_new_port_int/disconnect.sh, $script_install_dir/disc_new_port_int/check_disco.sh, $script_install_dir/disc_new_port_int/reset_disc.sh, $script_install_dir/wanalyzer/tcs_wanc_menu.sh, $script_install_dir/wanalyzer/tcs_wanem_main.sh
 _SUDOERS_END
-exit
+echo "  . Configured $sudoers_file"
  
 # install web content
 echo
 echo "- Processing web content"
+do_checkdir $web_dir
 cd web_home
 # make a list of content without that pesky ./
 for f in $(find -print0 | xargs -0 -n1 | cut -c 3-)
 do
 	echo "  - Installing $f"
-        if [ -d "./$f" ] 
+    if [ -d "./$f" ] 
 	then
-        	if [ ! -d "$target_dir/$f" ] 
+    	if [ -d "$web_dir/$f" ] 
 		then
-			mkdir "$target_dir/$f"	
+			if [ ! -w "$web_dir/$f" ] 
+			then
+				echo "  ! $web_dir/ not writable"
+			fi
+		else
+			mkdir "$web_dir/$f"	
 		fi
 	else
-		cp "./$f" "$target_dir/"
+		cp "./$f" "$web_dir/"
 	fi
 done
 cd ..
-echo "  - Web content complete"
+echo "  . Web content complete"
 
 # install script content
 echo
 echo "- Processing script content"
+do_checkdir $script_install_dir
 cd root_home
-target_dir="/root"
 # make a list of content without that pesky ./
 for f in $(find -print0 | xargs -0 -n1 | cut -c 3-)
 do
 	echo "  - Installing $f"
-        if [ -d "./$f" ] 
+    if [ -d "./$f" ] 
 	then
-        	if [ ! -d "$target_dir/$f" ] 
+       	if [ -d "$script_install_dir/$f" ] 
 		then
-			mkdir "$target_dir/$f"	
+			if [ ! -w "$script_install_dir/$f" ] 
+			then
+				echo "  ! $script_install_dir/ not writable"
+			fi
+		else
+			mkdir "$script_install_dir/$f"	
 		fi
 	else
-		cp "./$f" "$target_dir/"
+		cp "./$f" "$script_install_dir/"
 	fi
 done
 cd ..
-echo "  - Script content complete"
+echo "  . Script content complete"
 
 echo
-echo "Setup complete"
+echo "Setup complete."
