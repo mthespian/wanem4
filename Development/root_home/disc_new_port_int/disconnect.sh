57243a386d7f2fc941341f29528cc0a95dd3e863
Tracy S. Fitch <tracy.s.fitch@gmail.com>
2020-06-01 12:13:40 -0500

    fix disconnect.sh incorrect /proc path for ip_conntrack_tcp_timeout_established

--- Development/root_home/disc_new_port_int/disconnect.sh
+++ Development/root_home/disc_new_port_int/disconnect.sh
@@ -1,8 +1,7 @@
 #/****************************************************************************/
-#/*                              TCS WANem                                   */
-
+#/*                                WANem 4                                   */
 #/****************************************************************************/
-#/*                   COPYRIGHT (c) 2007 TCS All Rights Reserved             */
+#/*        COPYRIGHT (c) 2020 Tracy S. Fitch All Rights Reserved             */
 #/*                                                                          */
 #/*                                                                          */
 #/* WANem is a WAN emulation tool Conceptualized and developed by Innovation */
@@ -12,13 +11,14 @@
 #/*                                                                          */
 #/****************************************************************************/
 #/****************************************************************************/
-#/*   Author       : Manoj Nambiar, TCS Innovation Lab Performance Engg.     */
-
-#/*   Date         : March 2007                                              */
-#/*   Synopsis     : disconnect.sh                                           */
-#/*   Description  :                                                         */
+#/*   Original Author : Manoj Nambiar, TCS Innovation Lab Performance Engg.  */
+#/*   Date            : March 2007                                           */
+#/*   Synopsis        : disconnect.sh                                        */
+#/*   Description     :                                                      */
 #/*                                                                          */
-#/*   Modifications:                                                         */
+#/*   Modifications   :                                                      */
+#/*     2020-06-01  Tracy S. Fitch                                           */
+#/*                 - netfilter/ no longer under ipv4 in /proc/sys/net/      */
 #/****************************************************************************/
 #/*                                                                          */
 
@@ -29,10 +29,10 @@ export PATH=$PATH:/usr/local/sbin:/sbin:/bin:/usr/bin
 trap clean_up SIGHUP SIGINT SIGTERM 
 
 function clean_up {
-	# Perform program exit housekeeping
-	echo "Brutus!"
-	iptables -F FORWARD
-	exit
+    # Perform program exit housekeeping
+    echo "Brutus!"
+    iptables -F FORWARD
+    exit
 }
 
 iptables -F FORWARD 
@@ -40,5 +40,5 @@ export LD_LIBRARY_PATH=/usr/local/lib
 modprobe ip_conntrack
 
 # assuming this does not change while I am running
-export timeout=`cat /proc/sys/net/ipv4/netfilter/ip_conntrack_tcp_timeout_established`
+export timeout=`cat /proc/sys/net/netfilter/ip_conntrack_tcp_timeout_established`
 nohup awk -f $WRKDIR/disco.awk -v net_ttl=$timeout -v disc_dir=$WRKDIR $WRKDIR/input.dsc &
